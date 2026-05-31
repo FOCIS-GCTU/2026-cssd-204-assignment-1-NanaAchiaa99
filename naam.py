e5e5cd3272f13c6726943a6e1b2c53a292ee85bc
NanaAchiaa99 <2425401173@live.gctu.edu.gh>
2026-05-31 22:40:07 +0000

    final commit

---
+++ naam.py
@@ -0,0 +1,32 @@
+# File: naam.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Nana Achiaa Amankwah-Mensah
+# STUDENT ID:  2425401174
+# Email: 2425401174@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Nana Achiaa Amankwah-Mensah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+
+    print()
+    print("...NAAM")
+    print()
+    print("...NN......NN............AAAA..............AAAA............MM......MM......")
+    print("...NNN.....NN...........AA..AA............AA..AA...........MMM....MMM......")
+    print("...NNNN....NN..........AA....AA..........AA....AA..........MMMM..MMMM......")
+    print("...NN.NN...NN..........AA....AA..........AA....AA..........MM.MMMM.MM......")
+    print("...NN..NN..NN..........AAAAAAAA..........AAAAAAAA..........MM..MM..MM......")
+    print("...NN...NN.NN..........AA....AA..........AA....AA..........MM......MM......")
+    print("...NN....NNNN..........AA....AA..........AA....AA..........MM......MM......")
+    print("...NN.....NNN..........AA....AA..........AA....AA..........MM......MM......")
+    print("...NN......NN....**....AA....AA....**....AA....AA....**....MM......MM....**")
+    print("...NN......NN....**....AA....AA....**....AA....AA....**....MM......MM....**")
+    print()
+
+
+main()
