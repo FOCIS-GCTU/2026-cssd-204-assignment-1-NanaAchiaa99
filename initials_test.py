c8ee073342ee4ddc52a319f37d4cc2f27c5494e5
NanaAchiaa99 <2425401173@live.gctu.edu.gh>
2026-05-31 22:41:12 +0000

    final commit1

--- initials_test.py
+++ initials_test.py
@@ -5,7 +5,7 @@ from initials import main
 
 def test_initials():
     try:
-        exists = os.path.exists("initials.py")
+        exists = os.path.exists("naam.py")
         assert exists == True
     except:
         sys.exit()
