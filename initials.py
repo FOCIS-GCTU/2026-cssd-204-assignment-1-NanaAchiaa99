eb9803bc95dc9f7b0dec918f02304e8e70776a61
NanaAchiaa99 <2425401173@live.gctu.edu.gh>
2026-05-31 23:35:28 +0000

    Add files via upload
    My assignment

---
+++ initials.py
@@ -0,0 +1,29 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Horace Kojo Damtse
+# STUDENT ID:  2425404571
+# Email: 2425404571@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Augustus Buckman, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+def main(): 
+  print()
+  print("...HKD")
+  print()  
+  print("...HHHH....HHHH........KKKK....KKKK........DDDDDDDD.........")
+  print("...HHHH....HHHH........KKK....KKK..........DDD...DDDD.......")
+  print("...HHHH....HHHH........KKK...KK............DDD....DDDD......")
+  print("...HHHH....HHHH........KK....KK............DDD.....DDDD.....")
+  print("...HHHHHHHHHHHH........KKKKKK..............DDD......DDD.....")
+  print("...HHHHHHHHHHHH........KK....KK............DDD.....DDDD.....")
+  print("...HHHH....HHHH........KK....KK............DDD....DDDD......")
+  print("...HHHH....HHHH........KK.....KKK..........DDD...DDDD.......")
+  print("...HHHH....HHHH...**...KKK.....KKK....**...DDD...DDD......**")
+  print("...HHHH....HHHH...**...KKKK....KKKK...**...DDDDDDDD.......**")
+  print()
+
+main()
